4b87eef980fd5ab970488695d87d9be064fedccd
lnxwizard <alperakca79@outlook.com>
2023-10-19 22:51:18 +0300

    Added enums

---
+++ enums/enums2.rs
@@ -0,0 +1,33 @@
+// enums2.rs
+//
+// Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
+// hint.
+
+use std::io::Stderr;
+
+#[derive(Debug)]
+enum Message {
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
